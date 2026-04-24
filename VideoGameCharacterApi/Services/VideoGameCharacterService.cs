29c67c5516e960964e38badbe3b8c29aed20efb0
Hjertelundhs <tobias.hjertelundh@hotmail.com>
2026-04-24 21:48:59 +0200

    Make the data come from sql database

--- VideoGameCharacterApi/Services/VideoGameCharacterService.cs
+++ VideoGameCharacterApi/Services/VideoGameCharacterService.cs
@@ -1,16 +1,11 @@
-﻿using VideoGameCharacterApi.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameCharacterApi.Data;
+using VideoGameCharacterApi.Models;
 
 namespace VideoGameCharacterApi.Services
 {
-    public class VideoGameCharacterService : IVideoGameCharacterService
+    public class VideoGameCharacterService(AppDbContext context) : IVideoGameCharacterService
     {
-
-        private static List<Character> characters = new List<Character>
-    {
-        new Character{Game = "Mario", Id = 1, Name = "Super Mario Bros", Role = "Hero"},
-        new Character{Game = "Link", Id = 2, Name = "The Legend of Zelda", Role = "Hero"},
-        new Character{Game = "Bowser", Id = 3, Name = "Super Mario Bros", Role = "Villain"},
-    };
         public Task<Character> AddCharacterAsync(Character character)
         {
             throw new NotImplementedException();
@@ -21,13 +16,12 @@ namespace VideoGameCharacterApi.Services
             throw new NotImplementedException();
         }
 
-        public async Task<List<Character>> GetAllCharactersAsync() => await Task.FromResult(characters);
-
+        public async Task<List<Character>> GetAllCharactersAsync() => await context.Characters.ToListAsync();
 
         public async Task<Character?> GetCharacterByIdAsync(int id)
         {
-            var result = characters.FirstOrDefault(c => c.Id == id);
-            return await Task.FromResult(result);
+            var result = await context.Characters.FindAsync(id);
+            return result;
         }
 
         public Task<bool> UpdateCharacterAsync(int id, Character character)
